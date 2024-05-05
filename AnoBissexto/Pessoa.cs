124c4d20f35e0649eb8fca6dbdb5262136659ce6
ccosta.anthony@gmail.com <ccosta.anthony@gmail.com>
2024-05-05 15:47:43 -0300

    Adicionar arquivos de projeto.

---
+++ AnoBissexto/Pessoa.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnoBissexto
+{
+    internal class Pessoa
+    {
+        string nome;
+        int idade;
+        string profissao;
+        Pessoa anterior;
+
+        public Pessoa(string nome, int idade, string profissao)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.profissao = profissao;
+        }
+
+        public void setAnterior(Pessoa aux)
+        {
+            this.anterior = aux;
+        }
+        public Pessoa getAnterior()
+        {
+            return this.anterior;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public void setProfissao(string profissao)
+        {
+            this.profissao = profissao;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public int getIdade()
+        {
+            return this.idade;
+        }
+        public string getProfissao()
+        {
+            return this.profissao;
+        }
+        public string getAnoBissexto()
+        {
+            int idade = 0, idadeaux = 0, anonascimento = 0;
+            idade = this.getIdade();
+            anonascimento = (DateTime.Now.Year - idade);
+            for (int i = 0; i <= idade; i++)
+            {
+                if (DateTime.DaysInMonth(anonascimento, 02) == 29)
+                {
+                    idadeaux++;
+                }
+                anonascimento++;
+            }
+            return " - Idade em Anos Bissextos: " + idadeaux;
+        }
+        public override string? ToString()
+        {
+            return "Nome: " + nome + " Idade: " + idade + " Profissão: " + profissao + getAnoBissexto();
+        }
+    }
+}
---
+++ AnoBissexto/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using AnoBissexto;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int opc = -1;
+        PilhaPessoa pilha = new PilhaPessoa();
+        do
+        {
+            Console.WriteLine("1 - Criar Pessoa e inserir na Pilha");
+            Console.WriteLine("2 - Retornar resultados da Pilha");
+            opc = int.Parse(Console.ReadLine());
+            switch (opc)
+            {
+                case 0:
+                    Console.WriteLine("Finalizando programa");
+                    Console.ReadKey();
+                    break;
+                case 1:
+                    pilha.push(criarPessoa());
+                    break;
+                case 2:
+                    Console.WriteLine(pilha.print());
+                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    Console.ReadKey();
+                    break;
+
+            }
+        } while (opc != 0);
+    }
+    static Pessoa criarPessoa()
+    {
+        Pessoa temp;
+        string nome, profissao;
+        int idade;
+        Console.WriteLine("Informe o nome da pessoa");
+        nome = Console.ReadLine();
+        Console.WriteLine("Informe a idade da pessoa");
+        idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe a profissao da pessoa");
+        profissao = Console.ReadLine();
+        temp = new Pessoa(nome, idade, profissao);
+
+        return temp;
+    }
+    static void retornarDados(Pessoa p)
+    {
+        int idade = 0, idadeaux = 0, anonascimento = 0;
+        Console.WriteLine("Nome: " + p.getNome());
+        Console.WriteLine("Idade: " + p.getIdade());
+        Console.WriteLine("Profissão: " + p.getProfissao());
+        idade = p.getIdade();
+        anonascimento = (DateTime.Now.Year - idade);
+
+        for (int i = 0; i <= idade; i++)
+        {
+            if (DateTime.DaysInMonth(anonascimento, 02) == 29)
+            {
+                idadeaux++;
+            }
+            anonascimento++;
+        }
+        Console.WriteLine("Idade em Anos Bissextos: " + idadeaux);
+    }
+}
